60cd35806a5a63b8ad868bda44697323b7833b53
Gifford Boake <giffboake@gmail.com>
2019-05-14 17:09:28 -0400

    added fetching of all SWAPI pages instead of just the first page for characters

--- client/src/components/Planets.js
+++ client/src/components/Planets.js
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 
 const PLANETS_QUERY = gql`
-    query PlanetsQuery {
-        planets {
-            name,
-            climate,
-            terrain
-        }
+  query PlanetsQuery {
+    planets {
+      name
+      climate
+      terrain
     }
+  }
 `
 
 const Planets = () => {
-    return (
-        <div>
-            <h2>Planets:</h2>
-            <Query query={PLANETS_QUERY} >
-                {
-                    ({loading, error, data}) => {
-                        if(loading) return <h4>Loading.....</h4>
-                        if(error) console.log('ERROR: ', error)
-                        console.log('Data: ', data)
-                        
-                        return <>
-                            {
-                                data.planets.map( planet => {
-                                    return <div><h3>{planet.name}:</h3> has a {planet.climate} climate.</div>
-                            })
-                            }   
-                        </>
-                    }
-                }
-            </Query>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h2>Planets:</h2>
+      <Query query={PLANETS_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <h4>Loading.....</h4>
+          if (error) console.log('ERROR: ', error)
+          console.log('Data: ', data)
+
+          return (
+            <>
+              {data.planets.map(planet => {
+                return (
+                  <div key={planet.name}>
+                    <h3>{planet.name}:</h3> has a {planet.climate} climate.
+                    <p>And has {planet.terrain.map(i => ` ${i} `)} terrain</p>
+                  </div>
+                )
+              })}
+            </>
+          )
+        }}
+      </Query>
+    </div>
+  )
 }
 export default Planets
--- schema.js
+++ schema.js
@@ -1,62 +1,148 @@
-const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLSchema, Gra} = require('graphql')
 const axios = require('axios')
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
+} = require('graphql')
 
 /*  Types   */
 //////////////
 
 //Planets
 const PlanetType = new GraphQLObjectType({
-    name: 'Planet',
-    fields: () => ({
-         name: {type: GraphQLString},
-         population: {type: GraphQLString},
-         climate: {type: GraphQLString},
-         terrain: {type: GraphQLString},
-         rotation_period: {type: GraphQLString},
-         orbital_period: {type: GraphQLString},
-         diameter: {type: GraphQLString},
-         gravity: {type: GraphQLString},
-         residents: {type : GraphQLList(GraphQLString)}
-
-    })
+  name: 'Planet',
+  description: 'The planet resource from SWAPI',
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      description: 'Name of the planet.'
+    },
+    population: { type: GraphQLString },
+    climate: {
+      type: new GraphQLList(GraphQLString),
+      resolve: planet => planet.climate.split(',').map(item => item.trim()),
+      description: 'The various climates of this planet'
+    },
+    terrain: {
+      type: new GraphQLList(GraphQLString),
+      resolve: planet => planet.terrain.split(',').map(item => item.trim()),
+      description: 'The various climates of the planet'
+    },
+    rotation_period: {
+      type: GraphQLInt,
+      resolve: planet => parseInt(planet.rotation_period),
+      description: 'Time in hours it takes for planet rotation.'
+    },
+    orbital_period: {
+      type: GraphQLInt,
+      resolve: planet => parseInt(planet.orbital_period),
+      description: 'Time in days it takes the planet to orbit its sun'
+    },
+    diameter: {
+      type: GraphQLInt,
+      resolve: planet => parseInt(planet.diameter),
+      description: 'The diameter in kilimeters'
+    },
+    gravity: { type: GraphQLString },
+    residents: { type: GraphQLList(GraphQLString) }
+  })
 })
 
 //Characters
 const CharacterType = new GraphQLObjectType({
-    name: "Character",
-    fields: () => ({
-        name: {type: GraphQLString}
-    })
+  name: 'Character',
+  fields: () => ({
+    name: { type: GraphQLString, description: 'Name of character' },
+    height: { type: GraphQLString, description: 'Height in cm' },
+    mass: { type: GraphQLString, description: 'Weight in kg' },
+    hair_color: {
+      type: GraphQLString,
+      description: 'Characters hair color if any'
+    },
+    skin_color: { type: GraphQLString },
+    eye_color: { type: GraphQLString },
+    birth_year: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    homeworld: {
+      type: GraphQLString,
+      resolve: planet => {
+        return axios
+          .get(`${planet.homeworld}`)
+          .then(res => res.data.name)
+          .catch(err => console.log(err))
+      }
+    }
+  })
 })
 
 //Movies
 
-
 //Root Query
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        planets : {
-            type: new GraphQLList(PlanetType),
-            resolve(parent, args){
-                return axios.get('https://swapi.co/api/planets')
-                    .then( res => res.data.results)
+  name: 'RootQueryType',
+  fields: {
+    planets: {
+      type: new GraphQLList(PlanetType),
+      resolve(parent, args) {
+        return axios
+          .get('https://swapi.co/api/planets')
+          .then(res => res.data.results)
+      }
+    },
+    planet: {
+      type: PlanetType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://swapi.co/api/planets/${args.id}`)
+          .then(res => res.data)
+      }
+    },
+    characters: {
+      type: new GraphQLList(CharacterType),
+      resolve(parent, args) {
+        return axios
+          .get('https://swapi.co/api/people')
+          .then(res => {
+            const count = Math.ceil(res.data.count / 10)
+            console.log('The count: ', count)
+            let pages = []
+            for (let i = 1; i <= count; i++) {
+              console.log('adding page: ', i)
+              pages.push(axios.get(`https://swapi.co/api/people/?page=${i}`))
             }
-        },
-        planet : {
-            type: PlanetType,
-            args: {
-                id: {type: GraphQLInt}
-            },
-            resolve(parent, args){
-                return axios.get(`https://swapi.co/api/planets/${args.id}`)
-                    .then( res => res.data)
-            }
-        }
+            return Promise.all(pages)
+          })
+          .then(res => {
+            const resultData = res
+              .map(page => page.data.results)
+              .reduce((prevm, curr) => prevm.concat(curr))
+
+            console.log('The results : ', resultData[1])
+            return resultData
+          })
+          .catch(err => console.log('Err: ', err))
+      }
+    },
+    character: {
+      type: CharacterType,
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://swapi.co/api/people/${args.id}`)
+          .then(res => res.data)
+      }
     }
+  }
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery
 })
